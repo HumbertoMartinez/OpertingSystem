4eda09fd0067a18265f27a407c15723c9fb5a91a
HumbertoMartinez <9hmartinez@stpatrick.org>
2016-10-12 13:57:31 -0500

    Almost finished, need to finish 10-12-16

---
+++ Operating-System-master/AppObject.js
@@ -0,0 +1,46 @@
+function App(name,memory,state){
+
+    //Declare your instance variables here.
+    this.memory
+    this.state
+    this.name
+
+    //Declare the instance function open here.
+    this.open = function(){
+    this.state = "active"
+
+
+
+
+
+    };
+    this.sleep = function(){
+      if this.state = "active"
+
+    }
+
+    this.active = function(){
+
+      
+    }
+    //Declare the instance function sleep here.
+
+
+
+    //Declare the instance function active here.
+
+
+
+    //Declare the instance function close here.
+
+
+
+
+}
+
+//Use the constructor to create 4 App objects below.
+
+
+
+//Declare an array named appList, and place all 4 of your apps in that array.
+//The order does not matter.
